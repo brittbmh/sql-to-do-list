218f1dcf39f0cf91a1defe8971fd228201c7b49c
Britt Carter <brittbmh@gmail.com>
2019-01-11 16:44:16 -0600

    server setup

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,22 @@
 console.log('js');
+
+$(document).ready(readyNow);
+
+function readyNow() {
+    console.log('jq');
+    getTasks();
+}
+
+function getTasks() {
+    $.ajax({
+        method: 'GET',
+        url: '/tasks'
+    }).then((result) => {
+        const listOfTasks = result;
+        console.log(listOfTasks);
+        
+    }).catch((error) => {
+        alert('initial task pull failed');
+        console.log(error);
+    })
+}
---
+++ server/routes/tasks.router.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const pg = require('pg');
+
+const Pool = pg.Pool;
+
+const pool = new Pool({
+    database: 'weekend-to-do',
+    host: 'localhost',
+    port: 5432,
+    max: 10,
+    idleTimeoutMillis: 10000
+});
+
+
+
+module.exports = router;
