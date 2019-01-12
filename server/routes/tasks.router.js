699b6a1afd237e01268562525cf02319751e29b7
Britt Carter <brittbmh@gmail.com>
2019-01-11 19:29:35 -0600

    add initial get on server

--- server/routes/tasks.router.js
+++ server/routes/tasks.router.js
@@ -5,12 +5,25 @@ const pg = require('pg');
 const Pool = pg.Pool;
 
 const pool = new Pool({
-    database: 'weekend-to-do',
+    database: 'weekend-to-do-app',
     host: 'localhost',
     port: 5432,
     max: 10,
     idleTimeoutMillis: 10000
 });
+
+router.get('/', (req, res) => {
+    console.log('In /tasks GET');
+    let queryText = `SELECT * FROM "tasks" ORDER BY "tasks";`;
+    pool.query(queryText).then((result) => {
+        console.log(result);
+        res.send(result.rows);        
+    }).catch((error) => {
+        console.log('Error in GET /tasks', error);
+        res.sendStatus(500);        
+    })
+});
+
 
 
 
