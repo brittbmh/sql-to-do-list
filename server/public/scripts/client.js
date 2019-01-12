0d340cffe4980cb51a093b3ff0cd3affd0327e18
Britt Carter <brittbmh@gmail.com>
2019-01-11 19:43:53 -0600

    created table of tasks and added bootstrap

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,3 +1,5 @@
+
+
 console.log('js');
 
 $(document).ready(readyNow);
@@ -14,7 +16,27 @@ function getTasks() {
     }).then((result) => {
         const listOfTasks = result;
         console.log(listOfTasks);
-        
+        for (task of listOfTasks) {
+            let status;
+            if(task.completed === true){
+                status = 'completed';
+            } else if (task.completed === false){
+                status = 'not done';
+            } else {
+                alert('can not complete request')
+            };
+            $('#task-list-body').append(`<tr>
+                                            <td>${task.task}</td>
+                                            <td>${status}</td>
+                                            <td>
+                                                <button class="update-status"
+                                                    data-taskid="${task.id}">Update</button>
+                                            </td>
+                                            <td>
+                                                <button class="remove-task"
+                                                    data-taskid="${task.id}">Delete</button>
+            </tr>`)
+        }
     }).catch((error) => {
         alert('initial task pull failed');
         console.log(error);
